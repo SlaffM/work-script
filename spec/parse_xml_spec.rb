1fc2106937bf544b90645b68ec9462fe49e4b652
Slaff <slavkamm@gmail.com>
2015-07-07 13:30:27 +0300

    refactor tests

--- spec/parse_xml_spec.rb
+++ spec/parse_xml_spec.rb
@@ -3,16 +3,48 @@ require '.\spec_helper'
 require '../modules/parse_xml'
 
 
-describe 'Variables' do
+describe 'Check module XML.' do
 
 	before :all do
     	@file = XML::XML.new(File.expand_path("../files/xml/variables_spec.xml"))
-    	@variables = XML::Variables.new(@file)
-	end
+    	@variables = XML::Variables.new(@file).variables
 
-	it { @file.get_doc.should be_a Nokogiri::XML::Document }
-	
-	it { @variables.should be_an_instance_of(XML::Variables) }
+	end
+=begin
+
+	describe 'Variable.' do
+
+		it 'first should be a variable' do 
+			@variables.first.should be_a XML::Variable
+		end
+
+	end
+=end
+
+	describe 'Variables.' do
+
+		it { @file.get_doc.should be_a Nokogiri::XML::Document }
+		
+		it { expect(@variables).to be_a_kind_of(Array) }
+
+
+		describe 'Array of variables.' do
+
+			it 'should valid' do
+
+				@variables.each do |variable|
+
+					variable.should be_a XML::Variable
+
+				end
+
+			end
+			
+				
+
+		end
+
+	end
 
 
 
