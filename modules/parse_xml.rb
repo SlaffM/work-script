c3dcba3764dff0323094e26e922fb70a73f8ef54
Slaff <slavkamm@gmail.com>
2015-07-15 10:27:01 +0300

    Variables. remove "return nil" from build_variables_list

--- modules/parse_xml.rb
+++ modules/parse_xml.rb
@@ -38,39 +38,80 @@ module XML
 
 	class Variable
 
-		attr_accessor :voltage_level, :vl_name, :panel, :system, :ka_name, :signal_name, :symbaddr, :variable_name, :signal_short_name
+		attr_reader :voltage_level, 
+					:vl_name, 
+					:panel, 
+					:system, 
+					:ka_name, 
+					:signal_name, 
+					:symbaddr, 
+					:variable_name, 
+					:signal_short_name
 
-		def initialize(variable)						
-			create_headers(variable)
+		def initialize(variable)	
+
+			@errors = []
+			create_headers(variable) unless variable.empty?
+
 		end
+
+		
+
 
 		def create_headers(variable)			
 		
-			tag_name = variable[:tagname]	
-
-			signal_short_name = variable[:name][variable[:name].rindex(".")+1..-1] if variable[:name].include?(".")							
+			tag_name 			= variable[:tagname]			
 							
-			@voltage_level 		= tag_name[0, 8].strip 						#класс напряжения
-			@vl_name 			= tag_name[8, 27].strip						#название линии
-			@panel 				= tag_name[35, 8].strip						#номер панели
-			@system 			= tag_name[43, 8].strip 					#система
-			@ka_name			= clear_mip_tag(tag_name[51, 27].strip)		#название ка (устройства)
-			@signal_name 		= tag_name[78, 48].strip 					#навзвание сигнала
-			@symbaddr 			= variable[:symbaddr]						#адрес 61850 (symbaddr)
-			@variable_name 		= variable[:name]							#адрес переменной (name) V460
-			@signal_short_name 	= signal_short_name							#короткое название сигнала
+			@voltage_level 		= get_voltage_level(tag_name)					#класс напряжения
+			@vl_name 			= get_vl_name(tag_name)							#название линии
+			@panel 				= get_panel(tag_name)						#номер панели
+			@system 			= get_system(tag_name) 						#система
+			@ka_name			= get_ka_name(tag_name)						#название ка (устройства)
+			@signal_name 		= get_signal_name(tag_name)						#навзвание сигнала
+			@symbaddr 			= get_symbaddr(variable[:symbaddr])					#адрес 61850 (symbaddr)
+			@variable_name 		= get_variable_name(variable[:name])					#адрес переменной (name) V460
+			@signal_short_name 	= get_signal_short_name(variable[:name])					#короткое название сигнала
 			
 		end	
 
+		def get_voltage_level(tag_name)
+			tag_name[0, 8].nil? ? "" : tag_name[0, 8].strip
+		end
+		def get_vl_name(tag_name)
+			tag_name[8, 27].nil? ? "" : tag_name[8, 27].strip
+		end
+		def get_panel(tag_name)
+			tag_name[35, 8].nil? ? "" : tag_name[35, 8].strip
+		end
+		def get_system(tag_name)
+			tag_name[43, 8].nil? ? "" : tag_name[43, 8].strip
+		end
+		def get_ka_name(tag_name)
+			tag_name[51, 27].nil? ? "" : clear_mip_tag(tag_name[51, 27])
+		end
+		def get_signal_name(tag_name)
+			tag_name[78, 48].nil? ? "" : tag_name[78, 48].strip
+		end
+		def get_symbaddr(name)
+			name.nil? ? "" : name
+		end
+		def get_variable_name(name)
+			name.nil? ? "" : name
+		end
+		def get_signal_short_name(name)
+			name.include?(".") ? name[name.rindex(".")+1..-1] : ""				
+		end
+
+
 		def clear_mip_tag(tag)	
-			tag.include?("МИП") ? tag[/\(.{1,}\)/].gsub(/\(|\)/, '') : tag
+			tag.include?("МИП") ? tag[/\(.{1,}\)/].gsub(/\(|\)/, '') : tag.strip
 		end
 
 		def is_variable_ready?
 			!@variable_name.start_with?("INT.") && @variable_name.include?("!") && @variable_name.end_with?(".P")
 		end
 
-		def get_vl_name	
+		def get_long_vl_name	
 			case @system
 			when "МПРЗА" then @symbaddr.include?("MD") ? "#{@voltage_level} #{@vl_name} #{@ka_name}" : "#{@voltage_level} #{@vl_name}"
 			when "АСУ ТП" then @ka_name.include?("ВЛ") ? "#{@voltage_level} #{@vl_name}" : "#{@voltage_level} #{@vl_name} #{@ka_name}"
@@ -79,7 +120,7 @@ module XML
 		end
 
 		def eql?(name)
-			get_vl_name == name
+			get_long_vl_name == name
 		end
 
 
@@ -92,7 +133,7 @@ module XML
 		end
 
 		def get_prefix_device_name
-			@variable_name[0..@variable_name.rindex(".")-1]
+			@variable_name.include?(".") ? @variable_name[0..@variable_name.rindex(".")-1] : ""
 		end
 
 	end
@@ -100,9 +141,10 @@ module XML
 
 	class Variables	
 
-		attr_accessor 	:variables
+		attr_reader 	:variables, :errors
 	
 		def initialize(file)	
+			@errors = []
 			@doc 		= file.get_doc
 			@variables 	= build_variables_list						
 		end			
@@ -115,23 +157,32 @@ module XML
 			xml.xpath(expression)
 		end
 
+		def set_error(name)
+			@errors << name
+		end
+
 		def build_variables_list
 
-			get_nodes(@doc).map do |i|				
-				
+			get_nodes(@doc).map do |i|
+
 				name_content = i.at_css('Name').nil? ? "" : i.at_css('Name').content
 				symbaddr_content = i.at_css('SymbAddr').nil? ? "" : i.at_css('SymbAddr').content
-				tagname_content = i.at_css('Tagname').nil? ? "" : i.at_css('Tagname').content				
-								
-				variable = 	{
-								name: name_content,
-								symbaddr: symbaddr_content,
-								tagname: tagname_content						
-							}	
-											
-				Variable.new(variable)				
+				tagname_content = i.at_css('Tagname').nil? ? "" : i.at_css('Tagname').content
 
-			end			
+				if tagname_content.nil? || tagname_content.empty?
+					set_error(name_content)	
+					nil						
+				else
+					variable = 	{
+									name: name_content,
+									symbaddr: symbaddr_content,
+									tagname: tagname_content						
+								}
+												
+					Variable.new(variable)		
+				end		
+
+			end.compact			
 
 		end	
 		
@@ -202,7 +253,7 @@ module XML
 											tagname: variable_tagname, 
 										}												
 						variable = Variable.new(variable_tag)
-						if vl_group[:name] == variable.get_vl_name
+						if vl_group[:name] == variable.get_long_vl_name
 							vl_group[:childgroups].each do |device_type|
 								if device_type[:group][:device] == variable.get_prefix_device_name
 										variable_xml.at_css('SystemModelGroup').content = 
@@ -278,7 +329,7 @@ module XML
 			end
 	
 			def get_list_groups	
-				@variables.map { |variable| { name: variable.get_vl_name, guid: "", childgroups: [] } }.uniq
+				@variables.map { |variable| { name: variable.get_long_vl_name, guid: "", childgroups: [] } }.uniq
 			end
 
 			def get_device_group(vl_group)
